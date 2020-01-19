3a2790ef611e6d8cbc75c4bb19a710a16a2247e3
Stefano Franchi <stefano.franchi@gmail.com>
2020-01-18 18:56:45 -0600

    Added parsing of command line arguments. Passing a globbed list of file not working yet

--- TCXHeartRateZones.py
+++ TCXHeartRateZones.py
@@ -31,29 +31,42 @@
 
 # Go on from here
 
-
-# Export a Garmin heart rate time series to csv
-# Create csv file in working directory
-
-import os, csv
+# import sys, argparse
+from argparse import ArgumentParser, SUPPRESS
 import lxml.etree as ET
 import numpy as np
 import pandas as pd
-# from pandas import Series
 
-# SET DIRECTORY
-cd = os.path.dirname(os.path.abspath(__file__))
+# Parsing command line arguments, using options for required zone arguments
+# Disable default help
+parser = ArgumentParser(description='Read heart rate data from (a list of) TCX files and output a normed distribution by zones.', add_help=False)
+required = parser.add_argument_group('required arguments')
+optional = parser.add_argument_group('optional arguments')
+
+# Add back help 
+optional.add_argument(
+    '-h',
+    '--help',
+    action='help',
+    default=SUPPRESS,
+    help='show this help message and exit'
+)
+
+required.add_argument("-z","--zones", help="A list of 2 or more numbers delimiting heart rate activity zones in the form 0, n, m, k", type=lambda s: [int(item) for item in s.split(',')], required=True)
+required.add_argument("file_list", help="One or more TCX or FIT files containing heart rate data for one or more activities", type=str)
+args = parser.parse_args()
+print("zones list: ", args.zones)
+print("file list: ", args.file_list)
+
 
 # LOAD XML FILE
 #testing on file:///home/stefano/Desktop/Firefox Downloads/activity_4445930224.tcx
 xmlfile='/home/stefano/Desktop/Firefox Downloads/activity_4445930224.tcx'
 #xmlfile = 'activity_4445930224.tcx'
-etree = ET.parse(os.path.join(cd, xmlfile))
+# etree = ET.parse(os.path.join(cd, xmlfile))
+etree = ET.parse(xmlfile)
 
-# DEFINING COLUMNS
-columns = ['time', 'heartrate']
-
-# Defining namespaces
+# All non-default namespaces defined in Garmin's TCX files, for future reference 
 #NSMAP = {"ns5" : "http://www.garmin.com/xmlschemas/ActivityGoals/v1",
          #"ns3" : "http://www.garmin.com/xmlschemas/ActivityExtension/v2",
          #"ns2" : "http://www.garmin.com/xmlschemas/UserProfile/v2",
@@ -66,10 +79,9 @@ NSMAP = {"tcd" : "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"}
 
 # Extract heartrate data and convert to integers 
 heartrates = np.array(etree.xpath('.//tcd:HeartRateBpm/tcd:Value/text()', namespaces={"tcd" : "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"}), dtype=np.int32)
-# heartrate_series  = np.array(heartrates, dtype=np.int32)                                
 
-# Define bins, labels, and proceed to bin the heartrate series
-# For this example we use 5 zones plus zone 0:
+# Define bins and labels, and proceed to bin the heartrate series
+# For this example we use 5 real zones plus zone 0:
 #  Z0 -->   0 to 100
 #  Z1 --> 101 to 123
 #  Z2 --> 124 to 136
@@ -90,4 +102,4 @@ normed_heartrates = binned_heartrates.div(binned_heartrates.sum())
 # print(normed_heartrates) 
 
 # return csv output with zones,frequency columns  
-print(normed_heartrates.to_csv(columns=False))
+print(normed_heartrates.to_csv(header=False))
